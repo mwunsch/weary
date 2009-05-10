c0e7489520ed958761bdab0d67d232c01eef3716
Mark Wunsch <mark@markwunsch.com>
2009-05-10 04:55:50 -0400

    Following redirects. Wrapping Net::HTTP

--- request.rb
+++ request.rb
@@ -8,55 +8,81 @@ require 'core_extensions'
 require 'response'
 
 
-
-class Request
-
-  # url is a String
-  # uri is a Uri
-  attr_reader :url, :method, :http_verb
-  attr_accessor :uri, :options
+module Weary
+  class Request
+    
+    attr_reader :uri
+    attr_accessor :options
   
-  def initialize(http_verb, url, options={})
-    @http_verb = http_verb
-    self.uri = url
-    self.options = options
-  end
+    def initialize(http_verb, url, options={})
+      self.method = http_verb
+      self.uri = url
+      self.options = options
+    end
   
-  def uri=(url)
-    @uri = URI.parse(url)
-  end
+    def uri=(url)
+      @uri = URI.parse(url)
+    end
+    
+    def method=(http_verb)
+      @http_verb = case http_verb
+        when :get, :GET, /\bget\b/i
+          :get
+        when :post, :POST, /\bpost\b/i
+          :post
+        when :put, :PUT, /\bput\b/i
+          :put
+        when :delete, :del, :DELETE, :DEL, /\bdelete\b/i
+          :del
+        when :head, :HEAD, /\bhead\b/i
+          :head
+        else
+          raise ArgumentError, "Only GET, POST, PUT, DELETE, and HEAD methods are supported"
+      end
+    end
+    
+    def method
+      @http_verb
+    end
   
-  def perform
-    req = http.request(request, options.to_params)
-    Response.new(req)
-  end
-  
-  def request
-    case @http_verb
-      when :get, :GET, /\bget\b/i
-        Net::HTTP::Get.new(@uri.path)
-      when :post, :POST, /\bpost\b/i
-        Net::HTTP::Post.new(@uri.path)
-      when :put, :PUT, /\bput\b/i
-        Net::HTTP::Put.new(@uri.path)
-      when :delete, :del, :DELETE, :DEL, /\bdelete\b/i
-        Net::HTTP::Delete.new(@uri.path)
-      when :head, :HEAD, /\bhead\b/i
-        Net::HTTP::Head.new(@uri.path)
+    def perform
+      req = http.request(request, options.to_params)
+      response = Response.new(@http_verb, req)
+      if response.redirected?
+        response.follow_redirect
       else
-        raise ArgumentError, "Only GET, POST, PUT, DELETE, and HEAD methods are supported"
+        response
+      end
     end
-  end
   
-  private
-    def http
-      connection = Net::HTTP.new(@uri.host, @uri.port)
-      connection.use_ssl = @uri.is_a?(URI::HTTPS)
-      connection.verify_mode = OpenSSL::SSL::VERIFY_NONE if connection.use_ssl
-      connection
-    end
+    # I often typed in "process" when I meant "perform" so this made sense to me:
+    alias process perform
+    
+    private
+      def http
+        connection = Net::HTTP.new(@uri.host, @uri.port)
+        connection.use_ssl = @uri.is_a?(URI::HTTPS)
+        connection.verify_mode = OpenSSL::SSL::VERIFY_NONE if connection.use_ssl
+        connection
+      end
+    
+      def request
+        case @http_verb
+          when :get
+            Net::HTTP::Get.new(@uri.path)
+          when :post
+            Net::HTTP::Post.new(@uri.path)
+          when :put
+            Net::HTTP::Put.new(@uri.path)
+          when :del
+            Net::HTTP::Delete.new(@uri.path)
+          when :head
+            Net::HTTP::Head.new(@uri.path)
+        end
+      end
 
+  end
 end
-
-q = Request.new(:get, "http://github.com/api/v2/xml/user/show/mwunsch")
-q.perform
+# "http://github.com/api/v2/xml/user/show/mwunsch"
+q = Weary::Request.new(:get, "http://github.com/api/v2/xml/user/show/mwunsch")
+p q.perform.body
--- response.rb
+++ response.rb
@@ -1,5 +1,49 @@
-class Response
-  def initialize(http_response)
-    puts http_response.is_a?(Net::HTTPResponse)
+module Weary
+  class Response
+  
+    attr_reader :raw, :method, :code, :message, :header, :content_type, :cookie, :body  
+    alias mime_type content_type
+    
+    def initialize(http_method, http_response)
+      raise ArgumentError, "Must be a Net::HTTPResponse" unless http_response.is_a?(Net::HTTPResponse)
+      @raw = http_response
+      @method = http_method
+      @code = http_response.code.to_i
+      @message = http_response.message
+      @header = http_response.to_hash
+      @content_type = http_response.content_type
+      @cookie = http_response['Set-Cookie']
+      @body = http_response.body
+    end
+    
+    def redirected?
+      @raw.is_a?(Net::HTTPRedirection)
+    end
+    
+    def format
+      @format = case @content_type
+        when 'text/xml', 'application/xml'
+          :xml
+        when 'application/json', 'text/json', 'application/javascript', 'text/javascript'
+          :json
+        when 'text/html'
+          :html
+        when 'application/x-yaml', 'text/yaml'
+          :yaml
+        when 'text/plain'
+          :plain
+        else
+          nil
+      end
+    end
+    
+    def follow_redirect
+      if redirected?
+        Request.new(@method, @raw['location']).perform
+      else
+        nil
+      end
+    end
+          
   end
 end
