2774204e1a5d782faaa8d27ae9ee034173c2a84a
Mark Wunsch <mark@markwunsch.com>
2009-05-19 11:53:52 -0400

    Checking for format

--- lib/weary.rb
+++ lib/weary.rb
@@ -17,12 +17,18 @@ require 'weary/response'
 
 module Weary
   
-  attr_reader :format, :domain, :url, :resources
+  # Weary::Query quickly performs a :get request on a URL and parses the request
+  def self.Query(url)
+    req = Weary::Request.new(url, :get).perform
+    req.parse
+  end
+  
+  attr_reader :domain, :resources
   
   def on_domain(domain)
     parse_domain = URI.extract(domain)
     raise ArgumentError, 'The domain must be a URL.' if parse_domain.empty?
-    @domain = domain
+    @domain = parse_domain[0]
   end
 
   def as_format(format)
@@ -32,10 +38,6 @@ module Weary
   def authenticates_with(username,password)
     @username = username
     @password = password
-  end  
-
-  def construct_url(pattern)
-    @url = @domain + pattern
   end
 
   def declare_resource(resource, options={})
@@ -56,48 +58,49 @@ module Weary
       setup[resource] = yield
     else
       options[:via] = :get if options[:via].nil?
+      check_format(options)
       setup[resource] = options
     end
-
-    via = options[:via].to_s.downcase
-    format = @format ? @format : :json
-    format = options[:in_format] unless options[:in_format].nil?
-    
+      
     @resources << setup
   end
 
   private
     def get(method,options={})
-      action = {}
       options[:via] = :get
-      action[method.to_sym] = options
-      @method_array << action
+      check_format(options)
+      set_action(method,options)
     end
 
     def post(method,options={})
-      action = {}
       options[:via] = :post
-      action[method.to_sym] = options
-      @method_array << action
+      check_format(options)
+      set_action(method,options)
     end
 
     def put(method,options={})
-      options[:method] = method.to_s
       options[:via] = :put
-      options
+      check_format(options)
+      set_action(method,options)
     end
 
     def delete(method,options={})
-      options[:method] = method.to_s
       options[:via] = :delete
-      options
+      check_format(options)
+      set_action(method,options)
     end
-  
-  # Weary::Query quickly performs a :get request on a URL and parses the request
-  def self.Query(url)
-    req = Weary::Request.new(url, :get).perform
-    req.parse
-  end
+    
+    def check_format(hash)
+      if hash[:in_format].nil?
+        hash[:in_format] = (@format || :json)
+      end
+    end
+    
+    def set_action(method,options)
+      action = {}
+      action[method.to_sym] = options
+      @method_array << action
+    end
   
 end
 
