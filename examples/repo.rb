79b461a9ba394232517c87a6661a038f593d8b28
Mark Wunsch <mark@markwunsch.com>
2009-11-20 16:51:04 -0500

    Revising Twitter status example to use new API

--- examples/repo.rb
+++ examples/repo.rb
@@ -1,23 +1,18 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'weary')
 
-class Repository
-  extend Weary
+class Repository < Weary::Base
   
-  @gh_user = "mwunsch"
-  @gh_repo = "weary"
-  
-  domain "http://github.com/api/v2/"
-  format :yaml
+  def initialize(user, repo)
+    self.modify_resource(:show) do |r|
+      r.url = "http://github.com/api/v2/yaml/repos/show/#{user}/#{repo}"
+    end
+  end  
   
   get "show" do |r|
-    r.url = "<domain><format>/repos/show/#{@gh_user}/#{@gh_repo}"
-  end
-  
-  get "network" do |r|
-    r.url = "<domain><format>/repos/show/#{@gh_user}/#{@gh_repo}/network"
+    r.url = "http://github.com/api/v2/yaml/repos/show/__gh_user__/__gh_repo__"
   end
       
 end
 
-weary = Repository.new
-puts weary.show.body
+weary = Repository.new('mwunsch','weary')
+puts weary.show.perform.body
