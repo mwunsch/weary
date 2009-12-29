cfb62269c421229e0f7a671ca109c58a1576b308
Mark Wunsch <mark@markwunsch.com>
2009-12-29 11:14:31 -0500

    Batch callbacks

--- lib/weary/batch.rb
+++ lib/weary/batch.rb
@@ -1,11 +1,31 @@
 module Weary
   class Batch
     
-    attr_accessor :requests
+    attr_accessor :requests, :pool, :responses
     
     def initialize(*requests)
       @requests = requests.flatten
     end
     
+    def on_complete(&block)
+      @on_complete = block if block_given?
+      @on_complete
+    end
+    
+    def before_send(&block)
+      @before_send = block if block_given?
+      @before_send
+    end
+    
+    def perform(&block)
+      @on_complete = block if block_given?
+      @responses = []
+      @pool = []
+      before_send.call if before_send
+      requests.each {|req| @pool << req.perform! }
+      pool.each {|res| @responses << res.value }
+      on_complete.call if on_complete
+      responses
+    end
   end
 end
