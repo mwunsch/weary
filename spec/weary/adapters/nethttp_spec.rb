471f746db18441df0a0e006c3577b6a7fc90d2c8
Mark Wunsch <=>
2012-01-26 15:38:20 -0500

    Developing more of the NetHttp spec

--- spec/weary/adapters/nethttp_spec.rb
+++ spec/weary/adapters/nethttp_spec.rb
@@ -6,7 +6,7 @@ describe Weary::Adapter::NetHttp do
     @request = Weary::Request.new @url
   end
 
-  describe ".call" do
+  describe "::call" do
     before do
       stub_request(:get, @url).
          to_return(:status => 200, :body => "", :headers => {})
@@ -26,22 +26,72 @@ describe Weary::Adapter::NetHttp do
     end
   end
 
-  describe ".perform" do
-    it "performs the request through the connect method"
-    it "returns a Rack::Response"
-    it "yields the response to a block if given"
+  describe "::perform" do
+    it "performs the request through the connect method" do
+      described_class.stub(:connect) { Rack::Response.new("", 200, {})}
+      described_class.should_receive :connect
+      described_class.perform(@request.env)
+    end
+
+    it "returns a Rack::Response" do
+      stub_request(:get, @url).
+        to_return(:status => 200, :body => "", :headers => {})
+      described_class.perform(@request.env).should be_kind_of Weary::Response
+    end
+
+    it "yields the response to a block if given" do
+      stub_request(:get, @url).
+        to_return(:status => 200, :body => "", :headers => {})
+      code = nil
+      described_class.perform(@request.env) {|response| code = response.status }
+      code.should eql 200
+    end
   end
 
   describe "#perform" do
-    it "calls the class method `.perform`"
+    it "calls the class method `.perform`" do
+      described_class.stub(:perform) { Rack::Response.new("", 200, {})}
+      described_class.should_receive(:perform).with(@request.env)
+      described_class.new.perform(@request.env)
+    end
   end
 
-  describe ".connect" do
-    it "performs the http request"
-    it "returns a Rack::Response"
+  describe "::connect" do
+    before do
+      stub_request(:get, @url)
+    end
+
+    it "performs the http request" do
+      req = Rack::Request.new(@request.env)
+      described_class.connect(req)
+      a_request(:get, @url).should have_been_made
+    end
+
+    it "returns a Rack::Response" do
+      req = Rack::Request.new(@request.env)
+      described_class.connect(req).should be_kind_of Weary::Response
+    end
+
+    it "sets appropriate request headers" do
+      @request.headers 'User-Agent' => Weary::USER_AGENTS['Lynx 2.8.4rel.1 on Linux']
+      req = Rack::Request.new(@request.env)
+      described_class.connect(req)
+      a_request(:get, @url).with(:headers => @request.headers).should have_been_made
+    end
   end
 
-  describe ".socket" do
-    it "sets up the http connection"
+  describe "::normalize_request_headers" do
+    it "removes the HTTP_ prefix from request headers" do
+      @request.headers 'User-Agent' => Weary::USER_AGENTS['Lynx 2.8.4rel.1 on Linux']
+      headers = described_class.normalize_request_headers(@request.env)
+      headers.should have_key "USER_AGENT"
+    end
+  end
+
+  describe "::socket" do
+    it "sets up the http connection" do
+      req = Rack::Request.new(@request.env)
+      described_class.socket(req).should be_kind_of Net::HTTP
+    end
   end
 end
