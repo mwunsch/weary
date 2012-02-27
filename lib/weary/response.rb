0e38eaebea6a0b7cdb3f6d75e13a0afbe9f78c84
Mark Wunsch <mark@markwunsch.com>
2012-02-26 19:08:48 -0500

    Response is a read-only type of thing

--- lib/weary/response.rb
+++ lib/weary/response.rb
@@ -9,8 +9,36 @@ module Weary
 
     alias_method :__method__, :method
 
-    def method_missing(*args, &block)
-      @response.send(*args, &block)
+    def status
+      @response.status.to_i
+    end
+
+    def header
+      @response.header
+    end
+
+    def body
+      @response.body
+    end
+
+    def each(&iterator)
+      body.each(&iterator)
+    end
+
+    def finish
+      [status, header, self]
+    end
+
+    def success?
+      (200..299).include? status
+    end
+
+    def redirected?
+      (300..399).include? status
+    end
+
+    def length
+      @response.length
     end
 
     def call(env)
--- spec/weary/response_spec.rb
+++ spec/weary/response_spec.rb
@@ -1,10 +1,61 @@
 require 'spec_helper'
 
 describe Weary::Response do
+  describe "#status" do
+    subject { Weary::Response.new [""], 200, {'Content-Type' => 'text/plain'} }
+    it "returns a response code" do
+      subject.status.should eql 200
+    end
+  end
+
+  describe "#header" do
+    subject { Weary::Response.new [""], 200, {'Content-Type' => 'text/plain'} }
+    it "returns the headers as a hash" do
+      subject.header.should have_key 'Content-Type'
+    end
+  end
+
+  describe "#body" do
+    subject { Weary::Response.new ["Hi"], 200, {'Content-Type' => 'text/plain'} }
+    it "returns the body" do
+      subject.body.should eql ["Hi"]
+    end
+  end
+
+  describe "#each" do
+    subject { Weary::Response.new ["Hi"], 200, {'Content-Type' => 'text/plain'} }
+    it "calls #each on the body" do
+      iterated = false
+      subject.each {|body| iterated = body }
+      iterated.should eql "Hi"
+    end
+  end
+
   describe "#finish" do
     subject { Weary::Response.new [""], 200, {'Content-Type' => 'text/plain'} }
     it "provides a Rack tuple" do
       subject.finish.length.should be 3
+    end
+  end
+
+  describe "#success?" do
+    subject { Weary::Response.new ["Hi"], 200, {'Content-Type' => 'text/plain'} }
+    it "returns true if the request was successful" do
+      subject.success?.should be true
+    end
+  end
+
+  describe "#redirected?" do
+    subject { Weary::Response.new ["Hi"], 301, {'Content-Type' => 'text/plain'} }
+    it "returns true if the request was redirected" do
+      subject.redirected?.should be true
+    end
+  end
+
+  describe "#length" do
+    subject { Weary::Response.new ["Hi"], 301, {'Content-Type' => 'text/plain'} }
+    it "returns the content-length" do
+      subject.length.should eql subject.header['Content-Length'].to_i
     end
   end
 
