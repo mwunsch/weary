9a9db72c2672fccfe976f4c5fc89f9ea5fa2deb5
Mark Wunsch <mark@markwunsch.com>
2012-03-05 15:00:05 -0500

    Better regex match of content type in parsing

--- lib/weary/response.rb
+++ lib/weary/response.rb
@@ -52,7 +52,7 @@ module Weary
 
     def parse
       raise "The response does not contain a body" if body.nil? || body.empty?
-      raise "Unable to parse Content-Type: #{content_type}" unless content_type =~ /json$/
+      raise "Unable to parse Content-Type: #{content_type}" unless content_type =~ /json($|;.*)/
       MultiJson.decode body
     end
   end
