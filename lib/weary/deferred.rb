9acd144cc6b74ea6fca480ced53ae0b7e82d7a89
Mark Wunsch <mark@markwunsch.com>
2012-03-07 12:32:25 -0500

    Weary::Deferred is a class to construct asynchronous domain models

---
+++ lib/weary/deferred.rb
@@ -0,0 +1,35 @@
+require 'weary/response'
+
+module Weary
+  # Public: A handy abstract proxy class that is used to proxy a domain model
+  # in an asynchronous fashion. Useful if you're not interested in a
+  # Weary::Response object, but you want to pass that into a domain object
+  # when the response is available.
+  class Deferred < defined?(::BasicObject) ? ::BasicObject : ::Object
+
+    instance_methods.each {|m| undef_method(m) if m.to_s !~ /(?:^__|^nil\?$|^send$|^object_id$)/ } \
+      unless defined? ::BasicObject
+
+    attr_reader :callable
+
+    def initialize(future, model, callable=nil)
+      @future = future
+      @model = model
+      @callable = callable || ::Proc.new{|klass, response| klass.new(response) }
+      @target = nil
+    end
+
+    def complete?
+      !!@target
+    end
+
+    def method_missing(method, *args, &block)
+      if @model.method_defined? method
+        @target ||= @callable.call @model, @future.force
+        @target.send(method, *args, &block)
+      else
+        super
+      end
+    end
+  end
+end
---
+++ spec/weary/deferred_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+require 'weary/deferred'
+
+describe Weary::Deferred do
+  before :all do
+    @struct = Struct.new "Deferred", :response
+  end
+
+  before do
+    @request = Weary::Request.new "http://github.com/api/v2/json/repos/show/mwunsch/weary"
+    adapter = Class.new { include Weary::Adapter }
+    @request.adapter adapter
+  end
+
+  describe "::new" do
+    it "creates a new deffered proxy object around a model" do
+      deferred = described_class.new @request.perform, @struct
+      deferred.should be_instance_of @struct
+    end
+
+    it "with a factory method" do
+      deferred = described_class.new @request.perform, @struct, lambda {|model, response| response.status }
+      deferred.should eq 501
+    end
+  end
+
+  describe "#complete?" do
+    it "is true when the target is ready" do
+      deferred = described_class.new @request.perform, @struct
+      deferred.inspect
+      deferred.complete?.should be_true
+    end
+  end
+
+end
