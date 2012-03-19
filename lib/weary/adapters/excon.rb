5148eee4ba1b640f494ef4f9b6eb1c16ff8c50e3
Matt Wilson <mhw@hypomodern.com>
2012-03-19 19:20:44 -0400

    s/capitalize/upcase/

--- lib/weary/adapters/excon.rb
+++ lib/weary/adapters/excon.rb
@@ -30,7 +30,7 @@ module Weary
           :headers => normalize_request_headers(request.env),
           :method  => request.request_method,
           :path    => request.fullpath
-        }.merge(if request.request_method.capitalize == "GET"
+        }.merge(if request.request_method.upcase == "GET"
           { :query => request.params }
         else
           { :body => request.params.map { |k,v| "#{k}=#{v}" }.join("&") }
