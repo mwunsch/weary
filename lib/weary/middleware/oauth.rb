1ecf4695242d161588f2aa83cd10c0c35ca09c86
Mark Wunsch <mark@markwunsch.com>
2012-10-02 22:37:29 -0400

    Deconstruct query to hash by unescaping with ASCII-8BIT
    Fixes #25, and we don't have to get rid of SimpleOAuth.

--- lib/weary/middleware/oauth.rb
+++ lib/weary/middleware/oauth.rb
@@ -20,10 +20,26 @@ module Weary
 
       def sign(env)
         req = Rack::Request.new(env)
+        post_body = req.body.read
+        req.body.rewind
         SimpleOAuth::Header.new req.request_method,
                                 req.url,
-                                req.params,
+                                request_body_to_hash(post_body),
                                 @oauth
+      end
+
+      # Stolen from Rack::Utils
+      def request_body_to_hash(qs, d = nil)
+        params = Rack::Utils::KeySpaceConstrainedParams.new
+        default_sep = /[&;] */n
+
+        (qs || '').split(d ? /[#{d}] */n : default_sep).each do |p|
+          k, v = p.split('=', 2).map { |s| Rack::Utils.unescape(s, defined?(::Encoding) ? Encoding::BINARY : nil) }
+
+          Rack::Utils.normalize_params(params, k, v)
+        end
+
+        return params.to_params_hash
       end
     end
   end
